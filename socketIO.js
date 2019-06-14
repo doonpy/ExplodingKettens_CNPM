bd61db31670fc0eca8dfb1c3d610e70f5e6652e7
doonpy <nguyenduypoon@gmail.com>
2019-06-14 17:57:23 +0700

    Test

--- socketIO.js
+++ socketIO.js
@@ -9,58 +9,89 @@ function socketIO(server) {
     var io = socket(server);
     console.log("--> Địa chỉ server: " + ip.address())
 
-    //Namespace là Server
-    var device = io.on('connection', function (socket) {
-
-        console.log("--> Connected - Device: " + socket.id);
-
-        //lấy dữ liệu
-        socket.on('resInfomation', function (data) {
-            //Nhận được thì in ra thôi hihi.
-            console.log(data);
-            socketServer.emit('resInfomation', data);
-            // mositureController.addMositure(new Date(), data.mositure);
-            socketServer.emit('getMositure', { date: new Date(), date_formatted: moment(new Date).format("HH:mm:ss DD/MM/YYYY"), value: data.mositure });
-        })
-
-        socket.on("resResult", (data) => {
-            socketServer.emit('resResult', data);
-        });
-
-        //Khi socket client bị mất kết nối thì chạy hàm sau.
-        socket.on('disconnect', function () {
-            console.log("--> Disconnected - Device: " + socket.id) 	//in ra màn hình console cho vui
-        })
-    });
-
+    var deviceList = new Array();
+    var userList = new Array();
+    let timeCount;
 
     //Namespace là Server
-    var socketServer = io.of('/server')
-        .on('connection', function (socket) {
-            console.log("--> Connected - Server: " + socket.id);
+    io.on('connection', (socket) => {
+        //check và phân loại
+        socket.emit("getAuthorization", '');
 
+        //Đếm ngược nếu ko trả lời
+        timeCount = setTimeout(() => {
+            socket.disconnect();
+        }, 60 * 1000 * 1);
+
+        socket.on("resAuthorization", (data) => {
+            //Kết nối được thì clear
+            clearTimeout(timeCount);
+            data.id = socket.id;
+            //type: false - device; true - user
+            console.log(`--> Connected: Id: ${data.id} - Type: ${data.type}`);
+
+            if (data.type == false) {
+                deviceList.push(data);
+                socket.join('device');
+            }
+            if (data.type == true) {
+                userList.push(data);
+                socket.join('user');
+            }
+
+            // //Lắng nghe độ ẩm trả về
+            socket.on('resMositure', (data) => {
+                //Nhận được thì in ra thôi hihi.
+                data.time = data.time.replace(/\n/g, '');   //xóa \n thôi chứ hok có gì
+                console.log(data);
+
+                // mositureController.addMositure(new Date(), data.mositure);
+                socket.to('user').emit('resMositure', {
+                    date: new Date(),
+                    date_formatted: moment(new Date(data.time)).format("HH:mm:ss DD/MM/YYYY"),
+                    value: data.value
+                });
+            });
+
+            //Lắng nghe thông tin trả về
+            socket.on('resInfomation', (data) => {
+                console.log(data);
+            })
+
+            //Lắng nghe kết quả trả về
+            socket.on("resResult", (data) => {
+                console.log(data);
+                socketServer.emit('resResult', data);
+            });
+
+            //Lắng nghe lỗi trả về
+            socket.on("resError", (data) => {
+                console.log(data);
+            });
+
+            //Lắng nghe thông tin
             socket.on('getInfomation', () => {
-                device.emit('getInfomation', '');
+                socket.to('device').emit('getInfomation', '');
             });
 
             //Thay đổi chế độ tưới
-            socket.on('setPumbMode', () => {
-                device.emit('setPumbMode', '');
+            socket.on('setCommand', (data) => {
+                socket.to('device').emit('setCommand', data);
             })
 
-            //Bật tắt máy bơm
-            socket.on('controlPumb', () => {
-                device.emit('controlPumb', '');
+            //Update firmware
+            socket.on('updateFirmware', () => {
+                socket.to('device').emit('updateFirmware', '');
             })
 
+            //Khi socket client bị mất kết nối thì chạy hàm sau.
             socket.on('disconnect', function () {
-                // clearInterval(loop);
-                console.log("--> Disconnected - Server: " + socket.id) 	//in ra màn hình console cho vui
+                console.log(`--> Disconnected: ${data}`); 	//in ra màn hình console cho vui
 
             })
-
 
         });
-}
+    });
+};
 
 module.exports = socketIO;
--- src/mositure.js
+++ src/mositure.js
@@ -4,7 +4,12 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 $(document).ready(function () {
-    const socket = io('/server');
+    const socket = io();
+    socket.on('getAuthorization', () => {
+        let data = { "type": true, "userid": "NULL" };
+        socket.emit('resAuthorization', data);
+    });
+
     var dps = []; // dataPoints
     var chart = new CanvasJS.Chart("chartContainer", {
         title: {
@@ -53,46 +58,43 @@ $(document).ready(function () {
     }
 
     //Khai báo socket trên client
-    socket.on('getMositure', function (data) {
-        let list = $('#list');
-        let num = parseInt(list.children().length) + 1;
-        // var child = `<li>` + num + `.   Thời gian: ` + data.date + ` --- Độ ẩm: ` + data.value + `%</li>`;
-        let child = `<li class="row">${num}. ` +
-            `<p>Thời gian:&nbsp;</p>` +
-            `<p>${data.date_formatted}</p>` +
-            `<p>&nbsp;---&nbsp;</p>` +
-            `<p> Độ ẩm:&nbsp;</p>` +
-            `<p>${data.value}</p>` +
-            `<p>%</p>` +
-            `<p style="display:none">${data.date}</p></li>`;
-        list.append(child);
-        updateChart(data.date, parseInt(data.value));
-    });
+    // socket.on('resMositure', function (data) {
+    //     let list = $('#list');
+    //     let num = parseInt(list.children().length) + 1;
+    //     // var child = `<li>` + num + `.   Thời gian: ` + data.date + ` --- Độ ẩm: ` + data.value + `%</li>`;
+    //     let child = `<li class="row">${num}. ` +
+    //         `<p>Thời gian:&nbsp;</p>` +
+    //         `<p>${data.date_formatted}</p>` +
+    //         `<p>&nbsp;---&nbsp;</p>` +
+    //         `<p> Độ ẩm:&nbsp;</p>` +
+    //         `<p>${data.value}</p>` +
+    //         `<p>%</p>` +
+    //         `<p style="display:none">${data.date}</p></li>`;
+    //     list.append(child);
+    //     updateChart(data.date, parseInt(data.value));
+    // });
 
-    //Vòng lặp gửi yêu cầu lấy thông tin
-    socket.emit('getInfomation', '');
-    setInterval(() => {
-        socket.emit('getInfomation', '');
-    }, 5000);
+    // //Vòng lặp gửi yêu cầu lấy thông tin
+    // socket.emit('getInfomation', '');
 
-    socket.on('resInfomation', (data) => {
-        $("#pumbMode").text(data.pumbMode == true ? "Auto" : "Manual");
-        $("#pumbStatus").text(data.pumbStatus == 1 ? "On" : "Off");
-        if (data.pumbStatus == 1) {
-            $("#pumbControl").text('Tắt');
-            $("#pumbControl").removeClass('btn-success');
-            $("#pumbControl").addClass('btn-danger');
-        }
-        else {
-            $("#pumbControl").text('Mở');
-            $("#pumbControl").addClass('btn-success');
-            $("#pumbControl").removeClass('btn-danger');
-        }
-    });
+    // socket.on('resInfomation', (data) => {
+    //     $("#pumbMode").text(data.pumbMode == true ? "Auto" : "Manual");
+    //     $("#pumbStatus").text(data.pumbStatus == 1 ? "On" : "Off");
+    //     if (data.pumbStatus == 1) {
+    //         $("#pumbControl").text('Tắt');
+    //         $("#pumbControl").removeClass('btn-success');
+    //         $("#pumbControl").addClass('btn-danger');
+    //     }
+    //     else {
+    //         $("#pumbControl").text('Mở');
+    //         $("#pumbControl").addClass('btn-success');
+    //         $("#pumbControl").removeClass('btn-danger');
+    //     }
+    // });
 
-    socket.on('resResult', (data) => {
-        $("#thongbao").text(data.msg);
-    })
+    // socket.on('resResult', (data) => {
+    //     $("#thongbao").text(data.msg);
+    // })
 
     //Clear database
     $("#clearDB").click(() => {
@@ -107,29 +109,33 @@ $(document).ready(function () {
         })
     })
 
-    //Stop simulator
-    $("#switchCon").click(() => {
-        if (socket.connected) {
-            socket.disconnect();
-            $("#switchCon").removeClass('btn-dark');
-            $("#switchCon").addClass('btn-success');
-            $("#switchCon").text('Connect');
-        }
-        else {
-            socket.connect('/server');
-            $("#switchCon").addClass('btn-dark');
-            $("#switchCon").removeClass('btn-success');
-            $("#switchCon").text('Disconnect');
-        };
-        alert("Yep!");
-    })
+    // //Stop simulator
+    // $("#switchCon").click(() => {
+    //     if (socket.connected) {
+    //         socket.disconnect();
+    //         $("#switchCon").removeClass('btn-dark');
+    //         $("#switchCon").addClass('btn-success');
+    //         $("#switchCon").text('Connect');
+    //     }
+    //     else {
+    //         socket.connect('/server');
+    //         $("#switchCon").addClass('btn-dark');
+    //         $("#switchCon").removeClass('btn-success');
+    //         $("#switchCon").text('Disconnect');
+    //     };
+    //     alert("Yep!");
+    // })
 
-    //Thay đổi mode
-    $("#changemode").click(() => {
-        socket.emit('setPumbMode', '');
-    });
+    // //Thay đổi mode
+    // $("#changemode").click(() => {
+    //     socket.emit('setPumbMode', '');
+    // });
 
-    $("#pumbControl").click(() => {
-        socket.emit('controlPumb', '');
-    });
+    // $("#pumbControl").click(() => {
+    //     socket.emit('controlPumb', '');
+    // });
+
+    // $("#updateFW").click(() => {
+    //     socket.emit('updateFW', '');
+    // })
 });
